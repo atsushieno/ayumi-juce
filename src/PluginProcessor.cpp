9501d3b7dcf378fd91e5c1072c2fbd785056ae92
atsushieno <atsushieno@gmail.com>
2021-09-10 05:27:52 +0900

    Add audio plugin parameters beyond MIDI messages.
    Now the plugin behaves more like typical VSTs...

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -17,7 +17,28 @@
 #define AYUMI_LV2_MIDI_CC_ENVELOPE_SHAPE 0x13
 #define AYUMI_LV2_MIDI_CC_DC 0x50
 
+#define AYUMI_PARAMETER_MIXER_0_INDEX 0
+#define AYUMI_PARAMETER_MIXER_1_INDEX 1
+#define AYUMI_PARAMETER_MIXER_2_INDEX 2
+#define AYUMI_PARAMETER_VOLUME_0_INDEX 3
+#define AYUMI_PARAMETER_VOLUME_1_INDEX 4
+#define AYUMI_PARAMETER_VOLUME_2_INDEX 5
+#define AYUMI_PARAMETER_PAN_0_INDEX 6
+#define AYUMI_PARAMETER_PAN_1_INDEX 7
+#define AYUMI_PARAMETER_PAN_2_INDEX 8
+#define AYUMI_PARAMETER_ENVELOPE_INDEX 9
+#define AYUMI_PARAMETER_ENVELOPE_SHAPE_INDEX 10
+#define AYUMI_PARAMETER_NOISE_INDEX 11
+#define AYUMI_PARAMETER_CLOCK_RATE_INDEX 12
+
 //==============================================================================
+
+juce::AudioParameterFloat* createParameter(const char* nameBase, int i, juce::NormalisableRange<float>& range, float def)
+{
+    auto name = juce::String::formatted("%s %d", nameBase, i);
+    return new juce::AudioParameterFloat(name, name, range, def);
+}
+
 AyumiAudioProcessor::AyumiAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -30,11 +51,22 @@ AyumiAudioProcessor::AyumiAudioProcessor()
                        )
 #endif
 {
+    for (int i = 0; i < 3; i++)
+        addParameter(createParameter("Mixer", i, mixerRange, 2.0f));
+    for (int i = 0; i < 3; i++)
+        addParameter(createParameter("Volume", i, volumeRange, 14.0f));
+    for (int i = 0; i < 3; i++)
+        addParameter(createParameter("Pan", i, panRange, 0.5f));
+    addParameter(new juce::AudioParameterFloat("Envelope", "Envelope", envelopeRange, 64.0f));
+    addParameter(new juce::AudioParameterFloat("EnvelopeShape", "EnvelopeShape", envelopeShapeRange, 14.0f));
+    addParameter(new juce::AudioParameterFloat("NoiseFrequency", "NoiseFrequency", noiseFreqRange, 0.0f));
+    // FIXME: enable once this got to make more sense.
+    //addParameter(new juce::AudioParameterFloat("ClockRate", "ClockRate", clockRange, 2000000.0f));
+
+    addListener(this);
 }
 
-AyumiAudioProcessor::~AyumiAudioProcessor()
-{
-}
+AyumiAudioProcessor::~AyumiAudioProcessor() = default;
 
 //==============================================================================
 const juce::String AyumiAudioProcessor::getName() const
@@ -101,29 +133,26 @@ void AyumiAudioProcessor::changeProgramName (int index, const juce::String& newN
 //==============================================================================
 void AyumiAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    if (ayumi.state.magic_number != AYUMI_JUCE_STATE_MAGIC_NUMBER)
+        ayumi.reset();
 
     ayumi.active = false;
-    ayumi.sample_rate = sampleRate;
-    ayumi_configure(&ayumi.impl, 1, 2000000, (int) sampleRate);
-    ayumi_set_noise(&ayumi.impl, 4); // pink noise by default
+    ayumi.sample_rate = (int) sampleRate;
+    ayumi_configure(&ayumi.impl, 1, ayumi.state.clock_rate, ayumi.sample_rate);
+    ayumi_set_noise(&ayumi.impl, ayumi.state.noise_freq); // pink noise by default
+
 	for (int i = 0; i < 3; i++) {
-		ayumi.mixer[i] = 1 << 6; // tone, without envelope
-		ayumi_set_pan(&ayumi.impl, i, 0.5, 0); // 0(L)...1(R)
+		ayumi_set_pan(&ayumi.impl, i, ayumi.state.pan[i], 0); // 0(L)...1(R)
 		ayumi_set_mixer(&ayumi.impl, i, 1, 1, 0); // should be quiet by default
-		ayumi_set_envelope_shape(&ayumi.impl, 14); // see http://fmpdoc.fmp.jp/%E3%82%A8%E3%83%B3%E3%83%99%E3%83%AD%E3%83%BC%E3%83%97%E3%83%8F%E3%83%BC%E3%83%89%E3%82%A6%E3%82%A7%E3%82%A2/
-		ayumi_set_envelope(&ayumi.impl, 0x40); // somewhat slow
-		ayumi_set_volume(&ayumi.impl, i, 14); // FIXME: max = 14?? 15 doesn't work
+		ayumi_set_volume(&ayumi.impl, i, ayumi.state.volume[i]);
 	}
+    ayumi_set_envelope_shape(&ayumi.impl, ayumi.state.envelope_shape);
+    ayumi_set_envelope(&ayumi.impl, ayumi.state.envelope);
     ayumi.active = true;
 }
 
 void AyumiAudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-
     ayumi.active = false;
 }
 
@@ -179,7 +208,7 @@ void AyumiAudioProcessor::ayumi_process_midi_event(juce::MidiMessage &msg) {
 			goto note_off; // it is illegal though.
 		if (a->note_on_state[channel])
 			break; // busy
-		mixer = a->mixer[channel];
+		mixer = a->state.mixer[channel];
 		tone_switch = (mixer >> 5) & 1;
 		noise_switch = (mixer >> 6) & 1;
 		env_switch = (mixer >> 7) & 1;
@@ -194,8 +223,8 @@ void AyumiAudioProcessor::ayumi_process_midi_event(juce::MidiMessage &msg) {
 		tone_switch = (mixer >> 5) & 1;
 		noise_switch = (mixer >> 6) & 1;
 		// We cannot pass 8 bit message, so we remove env_switch here. Use BankMSB for it.
-		env_switch = (a->mixer[channel] >> 7) & 1;
-		a->mixer[channel] = bytes[1];
+		env_switch = (a->state.mixer[channel] >> 7) & 1;
+		a->state.mixer[channel] = bytes[1];
 		ayumi_set_mixer(&a->impl, channel, tone_switch, noise_switch, env_switch);
 		break;
 	case CMIDI2_STATUS_CC:
@@ -205,26 +234,28 @@ void AyumiAudioProcessor::ayumi_process_midi_event(juce::MidiMessage &msg) {
 			tone_switch = mixer & 1;
 			noise_switch = (mixer >> 1) & 1;
 			env_switch = (mixer >> 2) & 1;
-			a->mixer[channel] = bytes[1];
+			a->state.mixer[channel] = bytes[1];
 			ayumi_set_mixer(&a->impl, channel, tone_switch, noise_switch, env_switch);
 			break;
 		case CMIDI2_CC_PAN:
-			ayumi_set_pan(&a->impl, channel, bytes[2] / 128.0, 0);
+            a->state.pan[channel] = (float) bytes[2] / 128.0f;
+			ayumi_set_pan(&a->impl, channel, a->state.pan[channel], 0);
 			break;
 		case CMIDI2_CC_VOLUME:
-			ayumi_set_volume(&a->impl, channel, (bytes[2] > 119 ? 119 : bytes[2]) / 8); // FIXME: max is 14?? 15 doesn't work
+            a->state.volume[channel] = (bytes[2] > 119 ? 119 : bytes[2]) / 8;
+			ayumi_set_volume(&a->impl, channel, a->state.volume[channel]); // FIXME: max is 14?? 15 doesn't work
 			break;
 		case AYUMI_LV2_MIDI_CC_ENVELOPE_H:
-			a->envelope = (a->envelope & 0x3FFF) + (bytes[2] << 14);
-			ayumi_set_envelope(&a->impl, a->envelope);
+			a->state.envelope = (a->state.envelope & 0x3FFF) + (bytes[2] << 14);
+			ayumi_set_envelope(&a->impl, a->state.envelope);
 			break;
 		case AYUMI_LV2_MIDI_CC_ENVELOPE_M:
-			a->envelope = (a->envelope & 0xC07F) + (bytes[2] << 7);
-			ayumi_set_envelope(&a->impl, a->envelope);
+			a->state.envelope = (a->state.envelope & 0xC07F) + (bytes[2] << 7);
+			ayumi_set_envelope(&a->impl, a->state.envelope);
 			break;
 		case AYUMI_LV2_MIDI_CC_ENVELOPE_L:
-			a->envelope = (a->envelope & 0xFF80) + bytes[2];
-			ayumi_set_envelope(&a->impl, a->envelope);
+			a->state.envelope = (a->state.envelope & 0xFF80) + bytes[2];
+			ayumi_set_envelope(&a->impl, a->state.envelope);
 			break;
 		case AYUMI_LV2_MIDI_CC_ENVELOPE_SHAPE:
 			ayumi_set_envelope_shape(&a->impl, bytes[2] & 0xF);
@@ -293,15 +324,18 @@ void AyumiAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::
         ayumi_process_midi_event(msg);
 	}
 
-	for (int i = currentFrame; i < sample_count; i++) {
-		ayumi_process(&a->impl);
-		ayumi_remove_dc(&a->impl);
-        int nCh = totalNumOutputChannels - totalNumInputChannels;
-        if (nCh > 0)
-            auto &ptr = buffer.getWritePointer(totalNumInputChannels)[i] = (float) ayumi.impl.left;
-        if (nCh > 1)
-            auto &ptr = buffer.getWritePointer(totalNumInputChannels + 1)[i] = (float) ayumi.impl.right;
-	}
+    // If we support release envelope this optimization will have to change
+    if (ayumi.active && (ayumi.note_on_state[0] || ayumi.note_on_state[1] || ayumi.note_on_state[2])) {
+        for (int i = currentFrame; i < sample_count; i++) {
+            ayumi_process(&a->impl);
+            ayumi_remove_dc(&a->impl);
+            int nCh = totalNumOutputChannels - totalNumInputChannels;
+            if (nCh > 0)
+                auto &ptr = buffer.getWritePointer(totalNumInputChannels)[i] = (float) ayumi.impl.left;
+            if (nCh > 1)
+                auto &ptr = buffer.getWritePointer(totalNumInputChannels + 1)[i] = (float) ayumi.impl.right;
+        }
+    }
 }
 
 //==============================================================================
@@ -312,21 +346,88 @@ bool AyumiAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AyumiAudioProcessor::createEditor()
 {
-    return new AyumiAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor (*this);
 }
 
 //==============================================================================
 void AyumiAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    juce::MemoryOutputStream stream{destData, true};
+
+    for (int i = 0; i < 3; i++)
+        stream.writeInt(ayumi.state.mixer[i]);
+    for (int i = 0; i < 3; i++)
+        stream.writeInt(ayumi.state.volume[i]);
+    for (int i = 0; i < 3; i++)
+        stream.writeFloat(ayumi.state.pan[i]);
+    stream.writeInt(ayumi.state.envelope);
+    stream.writeInt(ayumi.state.envelope_shape);
+    stream.writeInt(ayumi.state.noise_freq);
+    stream.writeInt(ayumi.state.clock_rate);
+    stream.flush();
 }
 
 void AyumiAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    if (sizeInBytes < 52)
+        return; // insufficient space
+    juce::MemoryInputStream stream{data, (size_t) sizeInBytes, true};
+
+    for (int i = 0; i < 3; i++)
+        ayumi.state.mixer[i] = stream.readInt();
+    for (int i = 0; i < 3; i++)
+        ayumi.state.volume[i] = stream.readInt();
+    for (int i = 0; i < 3; i++)
+        ayumi.state.pan[i] = stream.readFloat();
+    ayumi.state.envelope = stream.readInt();
+    ayumi.state.envelope_shape = stream.readInt();
+    ayumi.state.noise_freq = stream.readInt();
+    ayumi.state.clock_rate = stream.readInt();
+
+    ayumi.state.magic_number = AYUMI_JUCE_STATE_MAGIC_NUMBER;
+}
+
+void AyumiAudioProcessor::audioProcessorParameterChanged(juce::AudioProcessor *processor, int parameterIndex,
+                                                         float newValue) {
+    if (parameterIndex <= AYUMI_PARAMETER_MIXER_2_INDEX) {
+        ayumi.state.mixer[parameterIndex % 3] = ((int) mixerRange.convertFrom0to1(newValue)) << 5;
+    } else if (parameterIndex <= AYUMI_PARAMETER_VOLUME_2_INDEX) {
+        auto vol = (int) volumeRange.convertFrom0to1(newValue);
+        ayumi.state.volume[parameterIndex % 3] = vol;
+        ayumi_set_volume(&ayumi.impl, parameterIndex % 3, vol);
+    } else if (parameterIndex <= AYUMI_PARAMETER_PAN_2_INDEX) {
+        ayumi.state.pan[parameterIndex % 3] = newValue;
+        ayumi_set_pan(&ayumi.impl, parameterIndex % 3, newValue, false);
+    } else {
+        int env, shape, noise, clock;
+        switch(parameterIndex) {
+            case AYUMI_PARAMETER_ENVELOPE_INDEX:
+                env = (int) envelopeRange.convertFrom0to1(newValue);
+                ayumi.state.envelope = env;
+                ayumi_set_envelope(&ayumi.impl, env);
+                break;
+            case AYUMI_PARAMETER_ENVELOPE_SHAPE_INDEX:
+                shape = (int) envelopeShapeRange.convertFrom0to1(newValue);
+                ayumi.state.envelope_shape = shape;
+                ayumi_set_envelope_shape(&ayumi.impl, shape);
+                break;
+            case AYUMI_PARAMETER_NOISE_INDEX:
+                noise = (int) noiseFreqRange.convertFrom0to1(newValue);
+                ayumi.state.noise_freq = noise;
+                ayumi_set_noise(&ayumi.impl, noise);
+                break;
+            case AYUMI_PARAMETER_CLOCK_RATE_INDEX:
+                clock = (int) clockRange.convertFrom0to1(newValue);
+                ayumi.state.clock_rate = clock;
+                ayumi_configure(&ayumi.impl, 1, clock, ayumi.sample_rate);
+            default: break;
+        }
+    }
+}
+
+void AyumiAudioProcessor::audioProcessorChanged (juce::AudioProcessor *processor, const juce::AudioProcessor::ChangeDetails &details)
+{
+    // what can be done here...?
 }
 
 //==============================================================================
--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -12,10 +12,12 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include "ayumi.h"
 
+#define AYUMI_JUCE_STATE_MAGIC_NUMBER 37564
+
 //==============================================================================
 /**
 */
-class AyumiAudioProcessor  : public juce::AudioProcessor
+class AyumiAudioProcessor  : public juce::AudioProcessor, private juce::AudioProcessorListener
 {
 public:
     //==============================================================================
@@ -57,20 +59,59 @@ public:
 
 private:
     typedef struct {
-        struct ayumi impl;
-        int mixer[3];
-        int32_t envelope;
-        int32_t pitchbend;
-        double sample_rate;
-        const char * bundle_path;
-        float* ports[3];
-        bool active;
-        bool note_on_state[3];
+        int magic_number{0}; // if it is loaded from state or initialized, then it is set.
+        // ayumi parameters (cached)
+        int mixer[3]{2, 2, 2};
+        int volume[3]{14, 14, 14};
+        float pan[3]{0.5, 0.5, 0.5};
+        int32_t envelope{0x40}; // somewhat slow
+        // see http://fmpdoc.fmp.jp/%E3%82%A8%E3%83%B3%E3%83%99%E3%83%AD%E3%83%BC%E3%83%97%E3%83%8F%E3%83%BC%E3%83%89%E3%82%A6%E3%82%A7%E3%82%A2/
+        int32_t envelope_shape{14};
+        int32_t noise_freq{0};
+        int32_t clock_rate{2000000};
+
+        inline void reset() {
+            magic_number = AYUMI_JUCE_STATE_MAGIC_NUMBER;
+            mixer[0] = mixer[1] = mixer[2] = 2;
+            volume[0] = volume[1] = volume[2] = 14;
+            pan[0] = pan[1] = pan[2] = 0.5;
+            envelope = 0x40;
+            envelope_shape = 14;
+            noise_freq = 0;
+            clock_rate = 2000000;
+        }
+    } AyumiState;
+
+    typedef struct {
+        struct ayumi impl{};
+        AyumiState  state{};
+        // non-persistent states
+        int32_t sample_rate{44100}; // stored for reconfiguration
+        bool active{false};
+        int32_t pitchbend{0};
+        bool note_on_state[3]{false, false, false};
+
+        inline void reset() {
+            state.reset();
+            sample_rate = 44100;
+            active = false;
+            pitchbend = 0;
+            note_on_state[0] = note_on_state[1] = note_on_state[2] = false;
+        }
     } AyumiContext;
 
     AyumiContext ayumi;
+    juce::NormalisableRange<float> mixerRange{0.0f, 8.0f, 1.0f};
+    juce::NormalisableRange<float> volumeRange{0.0f, 14.0f, 1.0f}; // FIXME: max = 14?? 15 doesn't work
+    juce::NormalisableRange<float> panRange{0.0f, 1.0f};
+    juce::NormalisableRange<float> envelopeRange{0.0f, 65535.0f, 1.0f, 0.2f};
+    juce::NormalisableRange<float> envelopeShapeRange{0.0f, 15.0f, 1.0f};
+    juce::NormalisableRange<float> noiseFreqRange{0.0f, 31.0f, 1.0f};
+    juce::NormalisableRange<float> clockRange{0.0f, 16777215.0f, 1.0f, 0.2f}; // does this range make sense?
 
     void ayumi_process_midi_event(juce::MidiMessage &msg);
+    void audioProcessorParameterChanged(AudioProcessor *processor, int parameterIndex, float newValue) override;
+    void audioProcessorChanged(AudioProcessor *processor, const AudioProcessor::ChangeDetails &details) override;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AyumiAudioProcessor)
